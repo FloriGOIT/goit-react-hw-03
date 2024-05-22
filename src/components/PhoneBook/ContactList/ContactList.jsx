7ca088790772772c91e3d3caf356af1ef70e8e66
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-05-22 18:39:32 +0300

    ok11

--- src/components/PhoneBook/ContactList/ContactList.jsx
+++ src/components/PhoneBook/ContactList/ContactList.jsx
@@ -3,17 +3,17 @@ import css from "./ContactList.module.css";
 
 
 export function ContactList({allContacts, toFilter, onClickDelete})
-{ const filteredList =  allContacts.filter(contact => contact.name.toLowerCase().includes(toFilter.toLowerCase()))
+{  if (allContacts.length === 0) { return <p>No contacts available.</p>; }
+
+   else {const filteredList =  allContacts.filter(contact => contact.name.toLowerCase().includes(toFilter.toLowerCase()))
   
-    return (<ul  className={css.listContactsWrap}>
-            {toFilter === "" && allContacts.map((contact, index) => <li key={contact.id}>
-                                                    <span><b>{index + 1}.</b>{contact.name}: {contact.number}</span>
-                                                    <button type="button" onClick = {() => onClickDelete(contact.id)}>Delete</button>
-                                                 </li>)}  
-            {toFilter !== "" && filteredList.map((contact, index) => <li key={contact.id}>
-                                                    <span><b>{index + 1}.</b>{contact.name}: {contact.number}</span>
-                                                    <button type="button" onClick = {() => onClickDelete(contact.id)}>Delete</button>
-                                                 </li>)}                                             
-         </ul>)
-}
+      const contactsToDisplay = (toFilter === "") ? allContacts : filteredList;
+
+  return ( <ul className={css.listContactsWrap}>
+               {contactsToDisplay.map((contact, index) => (<li key={contact.id}>
+                                                               <span><b>{index + 1}.</b> {contact.name}: {contact.number}</span>
+                                                               <button type="button" onClick={() => onClickDelete(contact.id)}>Delete</button>
+                                                          </li> ))}
+         </ul>);
+}}
 
