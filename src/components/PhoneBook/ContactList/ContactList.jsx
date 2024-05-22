dae8d079ea1a77778c02f88a4c0cc06f2a2c0fa8
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-05-22 18:42:52 +0300

    ok12

--- src/components/PhoneBook/ContactList/ContactList.jsx
+++ src/components/PhoneBook/ContactList/ContactList.jsx
@@ -3,13 +3,12 @@ import css from "./ContactList.module.css";
 
 
 export function ContactList({allContacts, toFilter, onClickDelete})
-{  if (allContacts.length === 0) { return <p>No contacts available.</p>; }
-
+{  
+   if(allContacts.length === "0"){ return <p>No contacts available.</p>}
    else {const filteredList =  allContacts.filter(contact => contact.name.toLowerCase().includes(toFilter.toLowerCase()))
-  
-      const contactsToDisplay = (toFilter === "") ? allContacts : filteredList;
+         const contactsToDisplay = (toFilter === "") ? allContacts : filteredList;
 
-  return ( <ul className={css.listContactsWrap}>
+  return (<ul className={css.listContactsWrap}>
                {contactsToDisplay.map((contact, index) => (<li key={contact.id}>
                                                                <span><b>{index + 1}.</b> {contact.name}: {contact.number}</span>
                                                                <button type="button" onClick={() => onClickDelete(contact.id)}>Delete</button>
