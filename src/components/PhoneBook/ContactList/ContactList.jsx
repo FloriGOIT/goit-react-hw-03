5762a3a66152d3896f8937d76cf1cbad09974c22
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-05-22 17:22:38 +0300

    ok1

--- src/components/PhoneBook/ContactList/ContactList.jsx
+++ src/components/PhoneBook/ContactList/ContactList.jsx
@@ -2,20 +2,18 @@ import css from "./ContactList.module.css";
 import PropTypes from "prop-types";
 
 
-
-
-export function ContactList({allContacts, toFilter,onClickDelete})
-{let filteredList = allContacts.filter(contact => contact.name.toLowerCase().includes(toFilter.toLowerCase()));
-  
-  return(<ul className={css.contactList}>
-            {toFilter === "" && allContacts.map((contactItem, index, array) => <li key={contactItem.id} >
-                                                <span><b>{index+1}.</b> {contactItem.name}:  {contactItem.number}</span>
-                                                <button type="button" onClick={() => onClickDelete(contactItem.id)}>Delete</button>
-                                            </li> )}
-            {toFilter !== "" && filteredList.map((contactItem, index, array) => <li key={contactItem.id} >
-                                                <span><b>{index+1}.</b> {contactItem.name}:  {contactItem.number}</span>
-                                                <button type="button" onClick={() => onClickDelete(contactItem.id)}>Delete</button>
-                                            </li> )}                                
-        </ul>)}
+export function ContactList({allContacts, toFilter, onClickDelete})
+{ const filteredList = () => {return allContacts.filter(contact => contact.name.toLowerCase().includes(toFilter.toLowerCase()))};  
+    return (<ul  className={css.listContactsWrap}>
+            {toFilter === "" && allContacts.map((contact, index) => <li key={contact.id}>
+                                                    <span><b>{index + 1}.</b>{contact.name}: {contact.number}</span>
+                                                    <button type="button" onClick = {() => onClickDelete(contact.id)}>Delete</button>
+                                                 </li>)}  
+            {toFilter !== "" && filteredList.map((contact, index) => <li key={contact.id}>
+                                                    <span><b>{index + 1}.</b>{contact.name}: {contact.number}</span>
+                                                    <button type="button" onClick = {() => onClickDelete(contact.id)}>Delete</button>
+                                                 </li>)}                                             
+            </ul>)
+}
 
 ContactList.propTypes = {allContacts:PropTypes.array, toFilter:PropTypes.string, onClickDelete: PropTypes.func}
