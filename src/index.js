9fabd5d1fbff35374c5fc39f34b9d68ed96e108d
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-05-22 18:21:54 +0300

    okkk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PhoneBook } from 'components/PhoneBook/PhoneBook';
-
+//import { PhoneBook } from 'components/PhoneBook/PhoneBook';
 import ImageSearch from 'components/ImageSearch/ImageSearch.jsx';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PhoneBook />
+    {/*<PhoneBook />*/}
     <ImageSearch />
     
   </React.StrictMode>
