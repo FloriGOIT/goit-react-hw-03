80b7ec7d516f19025921f86972d0c85cfb2e74d6
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-05-22 18:26:15 +0300

    okImg

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import { PhoneBook } from 'components/PhoneBook/PhoneBook';
-import ImageSearch from 'components/ImageSearch/ImageSearch.jsx';
+import { PhoneBook } from 'components/PhoneBook/PhoneBook';
+//import ImageSearch from 'components/ImageSearch/ImageSearch.jsx';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/*<PhoneBook />*/}
-    <ImageSearch />
+    <PhoneBook />
+    {/*<ImageSearch />*/}
     
   </React.StrictMode>
 );
